49d968bbfce392a91324e56a28c93c551f67cdb9
Matteo Zilio <siberia.italia@gmail.com>
2015-03-31 12:10:29 +0200

    Test - Comp

--- php/util.php
+++ php/util.php
@@ -331,5 +331,10 @@
 				</div>
   			</div>';
 	}
-
+	
+	function printCheckboxComp($res) {
+		while ($rowComp = mysqli_fetch_array($res)) {
+			echo "<label class=\"checkbox-inline\"><input type=\"checkbox\" value=\"" . $rowComp["NomeComp"] . ""\"/>" . $rowComp["NomeComp"] . "</label>";
+		}
+	}
 ?>
