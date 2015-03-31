3f4f4757e41d579fd0081cb9a1d67ef4ea2aecb7
Alex Cavallin <alex.cavallin@gmail.com>
2015-03-31 11:33:23 +0200

    aggiunta funzione printReq

--- php/printFonti.php
+++ php/printFonti.php
@@ -9,4 +9,14 @@
 		while ($row = mysqli_fetch_array($results)) 
 			echo '<option value="' . $row["NomeFonte"] . '">' . $row["NomeFonte"] . ' - ' . $row["DescrizioneFonte"] . '</option>';
 	}
+	function printReq($con) {
+		$query = "SELECT NomeReq, Descrizione
+				  FROM Requisiti";
+		$results = mysqli_query($con, $query);
+		if (empty($results))
+			return;
+
+		while ($row = mysqli_fetch_array($results)) 
+			echo '<label class="checkbox-inline"><input type="checkbox" name='Requisiti[]' value="' . $row["NomeReq"] . '" />' . $row["NomeReq"] . ' - ' . $row["Descrizione"] . '</label>'
+	}
 ?>
