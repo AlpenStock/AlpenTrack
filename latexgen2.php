48dbaeb3452e2d8e84611e0b50417aea27ec8de3
Alex Cavallin <alex.cavallin@gmail.com>
2015-04-19 15:15:11 +0200

    risolto problema celle infinite

--- latexgen2.php
+++ latexgen2.php
@@ -15,13 +15,22 @@ $tabella1="\bgroup
 \begin{longtable}{|l|p{10cm}|} \hline
 \\textbf{Requisito} & \\textbf{Classe}";
 $c="";
+$n = 0;
 while ($a = mysqli_fetch_row($trc)){
 	if($a[0]!=$v){
 		$tabella1.="\\\\\\hline \n";
 		$tabella1.="$a[0] & $a[1]"; 
 		$v=$a[0];
+		$n = 0;
 	}else{
-		$tabella1.="  \\newline ".$a[1];
+		if($n == 45){
+			$tabella1.="\\\\\\hline \n";
+			$tabella1.="$v & $a[1]"; 
+			$n = 0;
+		}else{
+			$tabella1.="  \\newline ".$a[1];
+			$n++;
+		}
 	}
 }
 $tabella1.="\\\\\\hline
@@ -35,16 +44,25 @@ $qry="SELECT c.NomeComp, r.NomeReq FROM Requisiti r NATURAL JOIN ReqComp rc NATU
 $tcr = mysqli_query($conn,$qry);
 $tabella2="\bgroup
 \def\arraystretch{1.8}
-\begin{longtable}{|l|p{10cm}|} \hline
+\begin{longtable}{|l|p{5cm}|} \hline
 \\textbf{Classe} & \\textbf{Requisito}";
 $c="";
+$n = 0;
 while ($a = mysqli_fetch_row($tcr)){
 	if($a[0]!=$v){
 		$tabella2.="\\\\\\hline \n";
 		$tabella2.="$a[0] & $a[1]"; 
 		$v=$a[0];
+		$n = 0;
 	}else{
-		$tabella2.=" \\newline ".$a[1];
+		if($n == 45){
+			$tabella2.="\\\\\\hline \n";
+			$tabella2.="$v & $a[1]";
+			$n = 0;
+		}else{
+			$tabella2.=" \\newline ".$a[1];
+			$n++;
+		}
 	}
 }
 $tabella2.="\\\\\\hline
